0e105996ba37c899f7130200ee700d5ec34f91cf
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-17 17:12:54 -0300

    Inclusão de texto

---
+++ back-end/src/routes.ts
@@ -0,0 +1,7 @@
+import {Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+  return res.json({ ok: true})
+})
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log('Servidor online!'))
+app.listen(3333, () => console.log('Servidor online!'));
