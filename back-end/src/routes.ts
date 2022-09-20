76bbaec35bc44cf1870da6f7bf3cd274ad8686e7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-19 21:06:23 -0300

    Inclusão de texto

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -3,7 +3,7 @@ import {Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-  return res.json({ nome: 'Sujeito Pizza' });
+  return res.json({ nome: ' Pizza' });
 });
 
 export { router };
