50bb350e2eeb20294b1c0e416f07e4ddf3eb25c5
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-20 21:14:02 -0300

    Inclusão de texto

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -10,8 +10,17 @@ app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if(err instanceof Error){
-    
+    // Se for uma instancia do tipo error
+    return res.status(400).json({
+      error: err.message
+    })
   }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error.'
+  })
+
 })
 
 app.listen(3333, () => console.log('Servidor online!'));
