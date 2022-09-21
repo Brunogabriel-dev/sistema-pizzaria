d9b8168452c321de4c0779bbcc1a431b75f9f810
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-20 21:34:37 -0300

    Inclusão de texto

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -3,7 +3,7 @@ import {Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-  throw new Error('Erro ao fazer essa requisição')
+  return res.json({ nome: 'Bruno' })
 });
 
 export { router };
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors';
+import cors from 'cors';
 
 import { router } from './routes'
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
