4622042b15bcc0c72f32e97740dc02240a2b2669
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-23 21:52:47 -0300

    Inclusão de texto

--- back-end/src/controllers/user/CreateUserController.ts
+++ back-end/src/controllers/user/CreateUserController.ts
@@ -1,8 +1,11 @@
 import {Request, response, Response} from 'express'
+import { CreateUserService } from '../../services/user/CreateUserService'
 
 class CreateUserController{
   async handle(req: Request, res: Response){
-    const {} = req.body;
+    const { name, email, password } = req.body;
+
+    const createUserService = new CreateUserService();
 
     return res.json({ ok:true })
   }
---
+++ back-end/src/services/user/CreateUserService.ts
@@ -0,0 +1,8 @@
+
+class CreateUserService{
+  async execute(){
+    return { ok: true }
+  }
+}
+
+export { CreateUserService }
