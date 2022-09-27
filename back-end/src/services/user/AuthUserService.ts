504bdce8bd3f447b7e5f785e34e6f612b76e187a
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-26 22:25:14 -0300

    Inclusão de texto

--- back-end/src/services/user/AuthUserService.ts
+++ back-end/src/services/user/AuthUserService.ts
@@ -24,6 +24,12 @@ class AuthUserService{
     // preciso verificar se a senha que ele mandou está correta.
     const passwordMatch = await compare(password, user.password)
 
+    if(!passwordMatch){
+      throw new Error("User/password incorrect")
+    }
+
+
+    // gerar um token JWT e devolver os dados do usuario como id, name e email
 
 
     return { ok: true }
