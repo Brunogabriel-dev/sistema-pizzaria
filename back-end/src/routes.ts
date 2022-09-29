74e9ef15f8c2b8dd680daa497a825663ff773a0b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-28 22:53:02 -0300

    Inclusão de texto

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import {} from './controllers/user/DetailUserController'
 
 const router = Router();
 
@@ -9,5 +10,7 @@ const router = Router();
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
+
+router.get('/me',)
 
 export { router };
