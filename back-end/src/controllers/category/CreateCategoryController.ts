5f800f4226bc97bc2a596762a4bc1574cfb31025
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-29 23:02:36 -0300

    Inclusão de texto

---
+++ back-end/src/controllers/category/CreateCategoryController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { CreateCategoryService } from '../../services/category/CreateCategoryService'
+
+class CreateCategoryController{
+  async handle(req: Request, res: Response){
+
+    const createCategoryService = new CreateCategoryService();
+
+    const category = await createCategoryService.execute()
+
+
+    return category;
+
+    
+  }
+}
+
+export { CreateCategoryController }
