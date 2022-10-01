42bd21653f4069a4157c78cf62518bb5f9122798
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-09-30 21:28:47 -0300

    Inclusão de texto

--- back-end/src/services/category/CreateCategoryService.ts
+++ back-end/src/services/category/CreateCategoryService.ts
@@ -1,8 +1,29 @@
 import prismaClient from "../../prisma";
 
+interface CategoryRequest{
+  name: string;
+}
+
 class CreateCategoryService{
-  async execute(){
-    return { ok: true }
+  async execute({ name }: CategoryRequest){
+    
+    if(name === ''){
+      throw new Error('Name invalid')
+    }
+
+    const category = await prismaClient.category.create({
+      data:{
+        name: name,
+      },
+      select:{
+        id: true,
+        name: true,
+      }
+    })
+
+
+    return category;
+
   }
 }
 
