f261b5394d168273ce0f9735c7a31e00a7cb1ea9
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-02 12:16:36 -0300

    Inclusão de texto

--- back-end/src/services/product/CreateProductService.ts
+++ back-end/src/services/product/CreateProductService.ts
@@ -1,7 +1,15 @@
 import prismaClient from "../../prisma";
 
+interface ProductRequest{
+  name: string;
+  price: string;
+  description: string;
+  banner: string;
+  category_id: string;
+}
+
 class CreateProductService{
-  async execute(){
+  async execute({name, price, description, banner, category_id}: ProductRequest){
 
   }
 }
