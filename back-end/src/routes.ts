b443c17282a1182420ad6bea3cc5717cb1fc0e60
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-02 12:42:30 -0300

    Inclusão de texto

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -7,6 +7,8 @@ import { DetailuserController } from './controllers/user/DetailUserController'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 
+import { CreateProductController } from './controllers/product/CreateProductController'
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router();
@@ -24,6 +26,6 @@ router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 //-- ROTAS PRODUCT
-
+router.post('/product', isAuthenticated, new CreateCategoryController().handle)
 
 export { router };
