9702ed8709b617f06846017ab7734b6b5f293fd2
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-02 12:37:32 -0300

    Inclusão de texto

--- back-end/src/controllers/product/CreateProductController.ts
+++ back-end/src/controllers/product/CreateProductController.ts
@@ -4,13 +4,21 @@ import { CreateProductService } from '../../services/product/CreateProductServic
 
 class CreateProductController{
   async handle(req: Request, res: Response){
-    const {} = req.body;
+    const { name, price, description, category_id } = req.body;
+
+    let banner = '';
 
     const createProductService = new CreateProductService();
 
     const product = createProductService.execute({
-
+      name,
+      price,
+      description,
+      banner,
+      category_id
     });
+
+    return res.json(product)
 
   }
 }
