2fd80fe4d31e795e72856c192f1150e7f1257c6e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-02 10:15:42 -0300

    Inclusão de texto

--- back-end/src/services/category/ListCategoryService.ts
+++ back-end/src/services/category/ListCategoryService.ts
@@ -3,6 +3,14 @@ import prismaClient from "../../prisma";
 class ListCategoryService{
   async execute(){
 
+    const category = await prismaClient.category.findMany({
+      select:{
+        id: true,
+        name: true,
+      }
+    })
+
+    return category;
   }
 }
 
