b68724f14eec834798b1d6140a98ef54a5c6b4bb
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-02 12:29:58 -0300

    Inclusão de texto

--- back-end/src/controllers/product/CreateProductController.ts
+++ back-end/src/controllers/product/CreateProductController.ts
@@ -1,2 +1,11 @@
-import {Request, Response} from 'express'
-import {} from '../../services/product/CreateProductService'
+import { Request, Response } from 'express'
+import { CreateProductService } from '../../services/product/CreateProductService'
+
+
+class CreateProductController{
+  async handle(){
+
+  }
+}
+
+export { CreateProductController }
