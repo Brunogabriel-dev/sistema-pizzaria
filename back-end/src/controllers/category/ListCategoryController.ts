34aae3698e86f293a886a165d73629a9db366d33
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-02 10:28:23 -0300

    Inclusão de texto

--- back-end/src/controllers/category/ListCategoryController.ts
+++ back-end/src/controllers/category/ListCategoryController.ts
@@ -1,2 +1,10 @@
 import {Request, Response } from 'express'
 import { ListCategoryService } from '../../services/category/ListCategoryService'
+
+class ListCategoryController{
+  async handle(req: Request, res: Response){
+
+  }
+}
+
+export { ListCategoryController }
