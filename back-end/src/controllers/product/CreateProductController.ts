13f729fdf88aefc7a59f08d5d3723a7d164a56d7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-04 22:24:30 -0300

    Inclusão de texto

--- back-end/src/controllers/product/CreateProductController.ts
+++ back-end/src/controllers/product/CreateProductController.ts
@@ -6,19 +6,26 @@ class CreateProductController{
   async handle(req: Request, res: Response){
     const { name, price, description, category_id } = req.body;
 
-    let banner = '';
-
     const createProductService = new CreateProductService();
 
-    const product = await createProductService.execute({
-      name,
-      price,
-      description,
-      banner,
-      category_id
-    });
+    if(!req.file){
+      throw new Error("error upload file")
+    }else{
 
-    return res.json(product)
+      const { originalname, filename } = req.file;
+
+      console.log(filename);
+
+      const product = await createProductService.execute({
+        name,
+        price,
+        description,
+        banner: '',
+        category_id
+      });
+  
+      return res.json(product)
+    }
 
   }
 }
--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
@@ -11,7 +12,11 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
+import uploadConfig from './config/multer'
+
 const router = Router();
+
+const upload = multer(uploadConfig.upload("./tmp"));
 
 //-- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
@@ -26,6 +31,6 @@ router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 //-- ROTAS PRODUCT
-router.post('/product', isAuthenticated, new CreateCategoryController().handle)
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export { router };
