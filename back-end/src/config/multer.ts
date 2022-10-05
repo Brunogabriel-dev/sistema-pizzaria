13e1814fad801b09c4a80220efb593a37b0a01e7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-04 21:43:03 -0300

    Inclusão de texto

---
+++ back-end/src/config/multer.ts
@@ -0,0 +1,14 @@
+import crypto from 'crypto';
+import multer from 'multer'
+
+import {extname, resolve} from 'path'
+
+export default{
+  upload(folder: string){
+    return{
+      storage: multer.diskStorage({
+        
+      })
+    }
+  }
+}
