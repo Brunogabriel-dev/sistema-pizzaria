ff8e7476529825ee567edf3b47c86237c8e0d23b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-10 22:34:22 -0300

    Inclusão de texto

--- back-end/src/controllers/order/CreateOrderController.ts
+++ back-end/src/controllers/order/CreateOrderController.ts
@@ -1,2 +1,10 @@
 import { Request, Response } from 'express';
 import { CreateOrderService } from '../../services/order/CreateOrderService'
+
+class CreateOrderController{
+  async handle(req: Request, res: Response){
+    const {} = req.body;
+  }
+}
+
+export { CreateOrderController }
