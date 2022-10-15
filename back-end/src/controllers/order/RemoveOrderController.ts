c4fcd531c50cfe2ddea6caf641471341c86cca08
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-14 21:23:58 -0300

    Inclusão de texto

--- back-end/src/controllers/order/RemoveOrderController.ts
+++ back-end/src/controllers/order/RemoveOrderController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
-import {} from '../../services/order/RemoveOrderService'
+import { RemoveOrderService } from '../../services/order/RemoveOrderService'
 
 class RemoveOrderController{
   async handle(req: Request, res: Response){
+    const order_id = req.query.order_id as string;
+
+    const removeOrder = new RemoveOrderService();
 
   }
 }
