75bc01503d3466bc80aeca8f98e4b8f0f361afc0
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-17 20:52:25 -0300

    Inclusão de texto

--- back-end/src/controllers/order/AddItemController.ts
+++ back-end/src/controllers/order/AddItemController.ts
@@ -3,6 +3,17 @@ import { AddItemService } from '../../services/order/AddItemService'
 
 class AddItemController{
   async handle(req: Request, res: Response){
+    const { order_id, product_id, amount } = req.body;
+
+    const addItem = new AddItemService();
+
+    const order = await addItem.execute({
+      order_id,
+      product_id,
+      amount
+    })
+
+    return res.json(order);
 
   }
 }
