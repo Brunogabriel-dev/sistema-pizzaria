794706564e932ee0f828bfa199c1d0e7cbdcf9dc
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-17 23:04:11 -0300

    Inclusão de texto

---
+++ back-end/src/services/order/DetailOrderService.ts
@@ -0,0 +1,20 @@
+import prismaClient from "../../prisma";
+
+interface DetailRequest{
+  order_id: string;
+}
+
+class DetailOrderService{
+  async execute({ order_id }: DetailRequest){
+
+    const orders = await prismaClient.item.findMany({
+      where:{
+        order_id: order_id
+      },
+      
+    })
+
+  }
+}
+
+export { DetailOrderService }
