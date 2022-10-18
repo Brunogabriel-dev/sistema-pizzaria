bf10760fe1ce00ee276595eea62cfc8bea87b3a5
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-17 21:17:32 -0300

    Inclusão de texto

---
+++ back-end/src/controllers/order/RemoveItemController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { RemoveItemService } from "../../services/order/RemoveItemService";
+
+class RemoveItemController{
+  async handle(req: Request, res: Response){
+
+  }
+}
+
+export { RemoveItemController }
