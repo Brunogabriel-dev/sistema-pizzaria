2d6de67765e3f5d7639326c7845472c3f9374aba
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-17 21:28:21 -0300

    Inclusão de texto

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -15,6 +15,7 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController'
 
 import { AddItemController } from './controllers/order/AddItemController'
+import { RemoveItemController } from './controllers/order/RemoveItemController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -46,5 +47,6 @@ router.post('./order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 
 export { router };
