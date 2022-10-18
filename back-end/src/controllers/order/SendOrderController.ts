083d06f3dbc60b8f8ffde06adb15760b064f3405
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-17 21:47:44 -0300

    Inclusão de texto

---
+++ back-end/src/controllers/order/SendOrderController.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from 'express'
+import {} from '../../services/order/SendOrderService'
+
+class SendOrderController{
+  async handle(){
+
+  }
+}
+
+export { SendOrderController }
