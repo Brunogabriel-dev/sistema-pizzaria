9b1e7c5d331c4804010ba10a42871b54442241ce
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-10-17 21:01:21 -0300

    Inclusão de texto

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -14,6 +14,8 @@ import { ListByCategoryController } from './controllers/product/ListByCategoryCo
 import { CreateOrderController } from './controllers/order/CreateOrderController'
 import { RemoveOrderController } from './controllers/order/RemoveOrderController'
 
+import { AddItemController } from './controllers/order/AddItemController'
+
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 import uploadConfig from './config/multer'
@@ -41,7 +43,8 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 
 //-- ROTAS ORDER
 router.post('./order', isAuthenticated, new CreateOrderController().handle)
-
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
 
 export { router };
